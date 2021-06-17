46884175bab900b7a081a96fe6f2a6298ef264a3
hallrizon <hallrizon@icloud.com>
2021-06-17 20:11:52 +0300

    Prototype

---
+++ patterns/prototype.py
@@ -0,0 +1,76 @@
+import copy
+from typing import Dict, Any, Union
+
+
+class SelfReferencingEntity:
+    def __init__(self):
+        self.parent = None
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+
+class SomeComponent:
+
+    def __init__(self, some_int, some_list_of_objects, some_circular_ref):
+        self.some_int = some_int
+        self.some_list_of_objects = some_list_of_objects
+        self.some_circular_ref = some_circular_ref
+
+    def __copy__(self):
+        some_list_of_objects = copy.copy(self.some_list_of_objects)
+        some_circular_ref = copy.copy(self.some_circular_ref)
+
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
+        new.__dict__.update(self.__dict__)
+
+        return new
+
+    def __deepcopy__(self, memo: dict = None):
+        memo = memo or {}
+
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
+        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
+
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
+        new.__dict__ = copy.deepcopy(self.__dict__, memo)
+
+        return new
+
+
+if __name__ == "__main__":
+
+    list_of_objects = [1, {1, 2, 3}, [1, 2, 3]]
+    circular_ref = SelfReferencingEntity()
+    component = SomeComponent(23, list_of_objects, circular_ref)
+    circular_ref.set_parent(component)
+
+    shallow_copied_component = copy.copy(component)
+
+    # Let's change the list in shallow_copied_component and see if it changes in
+    # component.
+    shallow_copied_component.some_list_of_objects.append("another object")
+    if component.some_list_of_objects[-1] == "another object":
+        print('same object')
+    else:
+        print('another object')
+
+    deep_copied_component = copy.deepcopy(component)
+
+    deep_copied_component.some_list_of_objects.append("one more object")
+    if component.some_list_of_objects[-1] == "one more object":
+        print('same object')
+    else:
+        print('another object')
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(10)
+    if 10 in deep_copied_component.some_list_of_objects[1]:
+        print('same object')
+    else:
+        print('another object')
